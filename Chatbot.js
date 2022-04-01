00e0de2296b350b04e5624cebb92b6248292bcbb
Gabriel Bibiano Santos <gabrielbibiano1@hotmail.com>
2022-04-01 20:55:14 -0300

    Em andamento
    Falta acertar o buttonSend e criar a janela para onde o texto vai.

--- Chatbot.js
+++ Chatbot.js
@@ -1,5 +1,7 @@
+var buttonSend = document.getElementById("send");
 var comprimentos1 = ["Oi", "Olá", "Hello"];
-var pessoa1 = "oi";
+var pessoa = document.getElementById("inserirTexto");
+var texto = pessoa;
 
 function write(){
     var comprimentos1random = comprimentos1[Math.floor(Math.random() * comprimentos1.length)];
@@ -10,10 +12,27 @@ function none(){
     console.log("Diga algo.");
 }
 
-if(pessoa1 == "oi"){
+buttonSend.onclick = function(){
+if(pessoa != "" & texto != ""){
     write();
-} else if(pessoa1 == ""){
+}else if(pessoa == ""){
     none();
-} else {
+}else {
     none();
+}
+}
+
+function confirm(){
+    let select = document.querySelector("#temas");
+    let optionsValue = select.options[select.selectedIndex];
+    let value = optionsValue.value;
+    if (value == "noticias"){
+        console.log("Funcionou!");
+    }else if (value == "previsaoDoTempo"){
+            console.log("Funcionou!");
+    }else if (value == "conhecimentosGerais"){
+            console.log("Funcionou!");
+    }else {
+        console.log("Escolha um tema");
+    }
 }
