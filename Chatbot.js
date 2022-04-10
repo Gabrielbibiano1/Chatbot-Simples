be6d3bcd8b060c41b93ac287ca7f691bea1ffa3f
Gabriel <gabrielbibiano1@hotmail.com>
2022-04-10 12:25:11 -0300

    Terceiro Commit

--- Chatbot.js
+++ Chatbot.js
@@ -1,7 +1,6 @@
 var buttonSend = document.getElementById("send");
+var buttonCancel = document.getElementById("cancel");
 var comprimentos1 = ["Oi", "Olá", "Hello"];
-var pessoa = document.getElementById("inserirTexto");
-var texto = pessoa;
 
 function write(){
     var comprimentos1random = comprimentos1[Math.floor(Math.random() * comprimentos1.length)];
@@ -9,18 +8,37 @@ function write(){
 }
 
 function none(){
-    console.log("Diga algo.");
+    var textArea = document.getElementById("inserirtexto");
+    textArea.firstChild.nodeValue = "";
+    console.log("Diga algo que seja possível de responder até o momento.");
 }
 
-buttonSend.onclick = function(){
-if(pessoa != "" & texto != ""){
-    write();
-}else if(pessoa == ""){
-    none();
-}else {
-    none();
+function searchKeyPress(e){
+    e = e || window.event;
+    if (e.keyCode == 13){
+        document.getElementById("send").click();
+        return false;
+    }
+    return true;
 }
+
+buttonSend.onclick = function receberTexto(){
+    var textoRecebido = document.getElementById("inserirtexto").value;
+    document.getElementById("mensagemenviada").value = textoRecebido;
+    document.getElementById("inserirtexto").innerHTML = "inserirtexto"[0].defaultValue;
+    if(textoRecebido == "oi"){
+        write();
+    }else if(textoRecebido == null || textoRecebido == "" || textoRecebido == undefined){
+        none();
+    }else {
+        none();
+    }
 }
+
+  buttonCancel.onclick = function limparTexto() {
+    document.getElementById("inserirtexto").textContent = ""
+    console.log("Campo de mensagem vazio");
+  }
 
 function confirm(){
     let select = document.querySelector("#temas");
